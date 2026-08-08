d608716fb7271e9afa5db1f10b3d6d4764407304
AdityaSharma147 <adityasharmax147@gmail.com>
2026-08-08 13:36:51 +0530

    updated servicesPage ui

--- Frontend/my-app/src/components/Facilities.jsx
+++ Frontend/my-app/src/components/Facilities.jsx
@@ -66,7 +66,7 @@ const Facilities = () => {
   return (
     <div className="bg-[#f8f0eb] py-16 px-4 md:px-20 sm:left-25">
       <div className="mx-auto max-w-7xl">
-        <div className="mb-12">
+        <div className="mb-12 bg-[#EEB76B] p-3 w-85 border-2  rounded border-amber-900 ">
           <p className="text-sm tracking-widest uppercase text-gray-500">
             Services
           </p>
